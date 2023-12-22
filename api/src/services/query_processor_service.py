3b24be3c996169f7c53478c360b169b080ee12be
kentata19 <ke.asada@raksul.com>
2023-12-22 11:47:57 +0900

    save history

--- api/src/services/query_processor_service.py
+++ api/src/services/query_processor_service.py
@@ -4,18 +4,22 @@ from services import BedrockService, KendraService, PromptCreatorService
 class QueryProcessorService:
     def __init__(self, query: str) -> None:
         self.query = query
+        self.kendra_service = KendraService()
+        self.bedrock_service = BedrockService()
 
     # Step1: Kendraにクエリを投げて、関連する文書を取得する
     # Step2: 取得した文書をもとにpromptを生成する
     # Step3: Bedrockにpromptを投げて、回答を取得する
     def process(self) -> str:
-        kendra_response = KendraService().search(self.query)
+        kendra_response = self.kendra_service.search(self.query)
         contents = [response.content for response in kendra_response]
         formatted_content = self.__format_content(contents)
         prompt = PromptCreatorService(
-            query=self.query, content=formatted_content
+            content=formatted_content, query=self.query
         ).create_prompt()
-        bedrock_response = BedrockService(prompt=prompt).call_endpoint()
+        bedrock_response = self.bedrock_service.call_endpoint(
+            prompt=prompt, query=self.query
+        )
         return bedrock_response
 
     @staticmethod
